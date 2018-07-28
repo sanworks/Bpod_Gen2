81369fecade999f33e256e73bb186a95bef71f85
Josh Sanders <josh@sanworks.io>
2018-07-28 16:27:42 -0400

    -Updated software update script

--- Functions/SoftwareUpdate/UpdateBpodSoftware.m
+++ Functions/SoftwareUpdate/UpdateBpodSoftware.m
@@ -1,4 +1,5 @@
 function UpdateBpodSoftware
+warning off
 % Check for open Bpod
 try
     evalin('base', 'BpodSystem;'); % BpodSystem is a global variable in the base workspace, representing the hardware
@@ -10,6 +11,14 @@ try
     end
 catch
 end
+% Create paths        
+BpodPath = fileparts(which('Bpod'));
+Path = struct;
+Path.BpodRoot = BpodPath;
+Path.ParentDir = fileparts(BpodPath);
+Path.LocalDir = fullfile(Path.ParentDir, 'Bpod Local');
+Path.Functions = fullfile(Path.BpodRoot, 'Functions');
+addpath(genpath(Path.Functions));
 % Check for latest version
 Ver = BpodSoftwareVersion;
 latestVersion = [];
@@ -24,16 +33,11 @@ if ~isempty(latestVersion)
         error(['No update required - you already have the latest stable version of Bpod: v' verString]);
     end
 end
-% Create paths        
-BpodPath = fileparts(which('Bpod'));
-Path = struct;
-Path.BpodRoot = BpodPath;
-Path.ParentDir = fileparts(BpodPath);
-Path.LocalDir = fullfile(Path.ParentDir, 'Bpod Local');
+
 TempDir = fullfile(Path.LocalDir, 'Temp');
 mkdir(TempDir); % Fails silently if it exists
 % Back up current Bpod software
-disp('Backing up existing software...')
+disp('Backing up current software...')
 BackupDir = fullfile(TempDir, 'Backup');
 mkdir(BackupDir);
 DateInfo = datestr(now, 30); 
@@ -57,8 +61,9 @@ delete(ZipFilePath);
 % Remove old files from path
 rmpath(genpath(Path.BpodRoot));
 % Delete old files (backed up previously)
-delete(BpodSystem.Path.BpodRoot);
-movefile(fullfile(DownloadDir, 'Bpod_Gen2-master'), Path.BpodRoot);
+dos_cmd = sprintf( 'rmdir /S /Q "%s"', Path.BpodRoot );
+[st, msg] = system(dos_cmd);
+movefile(fullfile(DownloadDir, 'Bpod_Gen2-master'), fullfile(Path.ParentDir, 'Bpod_Gen2'), 'f');
 SystemPath = fullfile(Path.BpodRoot, 'Functions');
 addpath(Path.BpodRoot);
 addpath(genpath(SystemPath));
