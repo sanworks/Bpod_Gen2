673db860e5b5371e9fac93352b7ff7c59581a59d
Josh Sanders <josh@sanworks.io>
2018-07-29 19:23:07 -0400

    -Fixes to soft code example state matrix

--- Examples/State Machines/USB Soft Codes/SoftCodeHandler_LatencyLoop.m
+++ Examples/State Machines/USB Soft Codes/SoftCodeHandler_LatencyLoop.m
@@ -1,4 +1,4 @@
 function SoftCodeHandler_LatencyLoop(SoftCode)
 if SoftCode == 1
-    SendBpodSoftCode(0);
+    SendBpodSoftCode(1);
 end
--- Functions/SendBpodSoftCode.m
+++ Functions/SendBpodSoftCode.m
@@ -4,7 +4,7 @@ if BpodSystem.Status.InStateMatrix == 1
     if Code <= BpodSystem.HW.n.SoftCodes && Code ~= 0
         BpodSystem.SerialPort.write(['~' Code-1], 'uint8');
     else
-        error(['Error: cannot send soft code ' num2str(Code) '; Soft codes must be in range: [0 ' num2str(BpodSystem.HW.n.SoftCodes) '].']) 
+        error(['Error: cannot send soft code ' num2str(Code) '; Soft codes must be in range: [1 ' num2str(BpodSystem.HW.n.SoftCodes) '].']) 
     end
 else
     error('Error sending soft code: Bpod must be running a trial.')
