ece95fdce00da24997e0d2380fee0b487ba75e03
gkane <gkane@rowland.harvard.edu>
2020-09-25 14:13:38 -0400

    CheckTimeOfDay: include minutes as fraction

--- Functions/CheckTimeOfDay.m
+++ Functions/CheckTimeOfDay.m
@@ -1,11 +1,12 @@
 function inRange = CheckTimeOfDay(startHour, stopHour)
 
     timeOfDay = datetime;
+    thisHour = timeOfDay.Hour + timeOfDay.Minute / 60;
 
     if startHour < stopHour
-        inRange = (timeOfDay.Hour > startHour) && (timeOfDay.Hour < stopHour);
+        inRange = (thisHour >= startHour) && (thisHour < stopHour);
     elseif startHour > stopHour
-        inRange = (timeOfDay.Hour > startHour) || (timeOfDay.Hour < stopHour);
+        inRange = (thisHour >= startHour) || (thisHour < stopHour);
     else
         inRange = true;
     end
