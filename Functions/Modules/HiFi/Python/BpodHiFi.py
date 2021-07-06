5f90694fdc00b6d8c45494d77fbad00d27e92590
Sanworks LLC <admin@sanworks.io>
2021-07-06 16:03:51 -0400

    -Added synth functions to BpodHiFi python class

--- Functions/Modules/HiFi/Python/BpodHiFi.py
+++ Functions/Modules/HiFi/Python/BpodHiFi.py
@@ -13,6 +13,12 @@ class BpodHiFi(object):
         self.maxWaves = InfoParams8Bit[2] # Number of sounds the device can store
         digitalAttBits = InfoParams8Bit[3]
         self._digitalAttenuation_dB = np.double(digitalAttBits)*-0.5
+        self._synthAmplitude = 0; # 0 = synth off, 1 = max
+        self._synthAmplitudeFade = 0; # Number of samples over which to ramp changes in synth amplitude (0 = instant change)
+        self._synthWaveform = 'WhiteNoise'; # WhiteNoise or Sine
+        self._synthFrequency = 1000; # Frequency of waveform (if sine)
+        self.Port.write(ord('F'), 'uint8', self._synthFrequency*1000, 'uint32') # Force SynthFrequency
+        confirm = self.Port.read(1, 'uint8')
         self._headphoneAmpEnabled = 0;
         self._headphoneAmpGain = 15;
         self.Port.write((ord('G'), 15), 'uint8'); # Set gain to non-painful initial level (range = 0-63)
@@ -56,6 +62,65 @@ class BpodHiFi(object):
         self._digitalAttenuation_dB = value
     
     @property
+    def synthAmplitude(self):
+        return self._synthAmplitude
+    
+    @synthAmplitude.setter
+    def synthAmplitude(self, value):
+        if not ((value >= 0) and (value <= 1)):
+            raise HiFiError('Error: Synth amplitude values must range between 0 and 1.')
+        amplitudeBits = round(value*32767);
+        self.Port.write(ord('N'), 'uint8', amplitudeBits, 'uint16')
+        confirm = self.Port.read(1, 'uint8');
+        if confirm != 1:
+             raise HiFiError('Error setting synthAmplitude: Confirm code not returned.')
+        self._synthAmplitude = value
+    
+    @property
+    def synthAmplitudeFade(self):
+        return self._synthAmplitudeFade    
+    
+    @synthAmplitudeFade.setter
+    def synthAmplitudeFade(self, value):
+        if not ((value >= 0) and (value <= 1920000)):
+            raise HiFiError('Error: Synth amplitude fade must range between 0 and 1920000 samples.')
+        self.Port.write(ord('Z'), 'uint8', value, 'uint32')
+        confirm = self.Port.read(1, 'uint8');
+        if confirm != 1:
+             raise HiFiError('Error setting synthAmplitudeFade: Confirm code not returned.')
+        self._synthAmplitudeFade = value
+    
+    @property
+    def synthWaveform(self):
+        return self._synthWaveform   
+    
+    @synthWaveform.setter
+    def synthWaveform(self, value):
+        validWaveforms = ['WhiteNoise','Sine']
+        if not value in validWaveforms:
+            raise HiFiError('Invalid value for synthWaveform: Valid values are WhiteNoise or Sine.')
+        waveformIndex = validWaveforms.index(value)
+        self.Port.write((ord('W'),waveformIndex), 'uint8')
+        confirm = self.Port.read(1, 'uint8');
+        if confirm != 1:
+             raise HiFiError('Error setting synthWaveform: Confirm code not returned.')
+        self._synthWaveform = value
+        
+    @property
+    def synthFrequency(self):
+        return self._synthFrequency
+    
+    @synthFrequency.setter
+    def synthFrequency(self, value):
+        if not ((value >= 20) and (value <= 80000)):
+            raise HiFiError('Error: Synth frequency must range between 0 and 80000 Hz.')
+        self.Port.write(ord('F'), 'uint8', value*1000, 'uint32')
+        confirm = self.Port.read(1, 'uint8')
+        if confirm != 1:
+             raise HiFiError('Error setting synthFrequency: Confirm code not returned.')
+        self._synthFrequency = value
+        
+    @property
     def headphoneAmpEnabled(self):
         return self._headphoneAmpEnabled
     
@@ -76,7 +141,7 @@ class BpodHiFi(object):
     
     @headphoneAmpGain.setter
     def headphoneAmpGain(self, value):
-        if not (value >= 0) and (value <= 63):
+        if not ((value >= 0) and (value <= 63)):
             raise HiFiError('Error: Headphone amp gain values must range between 0 and 63.')
         self.Port.write((ord('G'),value), 'uint8')
         confirm = self.Port.read(1, 'uint8');
@@ -101,7 +166,7 @@ class BpodHiFi(object):
         nEnvelopeSamples = envelope.shape[0]
         if nEnvelopeSamples > self.maxEnvelopeSamples:
             raise HiFiError('Error: AM Envelope cannot contain more than ' + str(self.maxEnvelopeSamples) + ' samples.')
-        if not (envelope >= 0).all() and (envelope <= 1).all():
+        if not ((envelope >= 0).all() and (envelope <= 1).all()):
             raise HiFiError('Error: AM Envelope values must range between 0 and 1.')
         self.Port.write((ord('E'), 1, ord('M')), 'uint8', nEnvelopeSamples, 'uint16', envelope, 'single')
         confirm = self.Port.read(2, 'uint8')
@@ -132,6 +197,10 @@ class BpodHiFi(object):
         'Port: ArCOMObject(' + self.Port.serialObject.port + ')'  + '\n'
         'samplingRate: ' + str(self.samplingRate) + '\n'
         'digitalAttenuation_dB: ' + str(self.digitalAttenuation_dB) + '\n'
+        'synthAmplitude: ' + str(self.synthAmplitude) + '\n'
+        'synthAmplitudeFade: ' + str(self.synthAmplitudeFade) + '\n'
+        'synthWaveform: ' + str(self.synthWaveform) + '\n'
+        'synthFrequency: ' + str(self.synthFrequency) + '\n'
         'headphoneAmpEnabled: ' + str(self.headphoneAmpEnabled) + '\n'
         'headphoneAmpGain: ' + str(self.headphoneAmpGain) + '\n'
         )
