a703a157e68295ead8120efea7e67357f7431ea4
George Stuyt <stugeor@hotmail.com>
2025-04-27 09:51:08 +1000

    Modify datetime char creation

--- Functions/+BpodLib/+calibration/+liquid/isotime.m
+++ Functions/+BpodLib/+calibration/+liquid/isotime.m
@@ -2,5 +2,6 @@ function datestring = isotime()
 % Get the current time in ISO format
 % :return datestring: the current time in ISO format
 % :rtype datestring: char
-datestring = char(datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss'));
+dt = datetime('now');
+datestring = char(dt, 'yyyy-MM-dd HH:mm:ss');
 end
