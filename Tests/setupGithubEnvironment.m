e4021f30c2539d33f0e64600149c08ec9a23bd86
George Stuyt <stugeor@hotmail.com>
2025-04-27 09:28:34 +1000

    Add pathing to test action

--- Tests/setupGithubEnvironment.m
+++ Tests/setupGithubEnvironment.m
@@ -7,14 +7,6 @@ mkdir(localDir)
 mkdir(calFolder)
 copyfile(fullfile(parentDir, 'Bpod_Gen2/Examples/Example Calibration Files'), calFolder)
 
-% GitHub Actions can't startup Bpod because of its GUI elements
-% Have to add Bpod's paths manually
-addpath(genpath(fullfile(parentDir, 'Bpod_Gen2/Functions')));
-disp('Current MATLAB path:');
-path
-
-disp('Can find bpodlib?')
-which('BpodLib.calibration.liquid.ValveDataClass')
 % Bpod EMU
 % EndBpod
 %% Run all of the tests
