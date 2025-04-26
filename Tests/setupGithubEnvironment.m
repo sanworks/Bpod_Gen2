be6ace570d8a8e403fcabeeb357769879fb3881f
George Stuyt <stugeor@hotmail.com>
2025-04-27 08:56:11 +1000

    Make Bpod startup once

--- Tests/setupGithubEnvironment.m
+++ Tests/setupGithubEnvironment.m
@@ -7,6 +7,8 @@ mkdir(localDir)
 mkdir(calFolder)
 copyfile(fullfile(parentDir, 'Bpod_Gen2/Examples/Example Calibration Files'), calFolder)
 
-
+% Startup bpod
+Bpod EMU
+EndBpod
 %% Run all of the tests
 % the workflow will run the tests
