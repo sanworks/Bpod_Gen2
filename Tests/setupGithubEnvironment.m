22a710b336e027348528d18895b110ff2d03d384
George Stuyt <stugeor@hotmail.com>
2025-04-27 08:50:55 +1000

    Initial commit

---
+++ Tests/setupGithubEnvironment.m
@@ -0,0 +1,12 @@
+%% Prepare GitHub environment
+% Create Bpod_Local folder
+parentDir = fileparts(pwd);
+localDir = fullfile(parentDir, 'Bpod Local');
+calFolder = fullfile(localDir, 'Calibration Files');
+mkdir(localDir)
+mkdir(calFolder)
+copyfile(fullfile(parentDir, 'Bpod_Gen2/Examples/Example Calibration Files'), calFolder)
+
+
+%% Run all of the tests
+% the workflow will run the tests
