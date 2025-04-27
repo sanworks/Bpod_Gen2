8c12c49c0d841e995b01a31c633cd6beee3f6c94
George Stuyt <stugeor@hotmail.com>
2025-04-27 10:18:46 +1000

    Revert datetime changes
    The problem was in the unit test

--- Functions/+BpodLib/+calibration/+liquid/isotime.m
+++ Functions/+BpodLib/+calibration/+liquid/isotime.m
@@ -2,7 +2,5 @@ function datestring = isotime()
 % Get the current time in ISO format
 % :return datestring: the current time in ISO format
 % :rtype datestring: char
-dt = datetime('now');
-datestring = char(dt, 'yyyy-MM-dd HH:mm:ss');
-datestring = datestr(now, 31);
+datestring = datetime('now', 'Format', 'yyyy-MM-dd HH:mm:ss');
 end
