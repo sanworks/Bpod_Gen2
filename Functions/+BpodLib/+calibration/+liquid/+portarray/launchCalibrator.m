06813e950d217c7e25cd600f2d29083dd59d7249
George Stuyt <stugeor@hotmail.com>
2025-04-29 14:26:25 +1000

    Add error for non-existent data

--- Functions/+BpodLib/+calibration/+liquid/+portarray/launchCalibrator.m
+++ Functions/+BpodLib/+calibration/+liquid/+portarray/launchCalibrator.m
@@ -2,4 +2,7 @@ function launchCalibrator(BpodSystem)
 % Launch a liquid calibrator with the port array
 
 savepath = fullfile(BpodLib.path.getPath(BpodSystem, 'liquidcalibration'), 'LiquidCalibration-PortArrays.json');
+if ~isempty(BpodSystem.CalibrationTables.PortArrays)
+    error('BpodLib:portarraylaunchCalibrator:NoData', "No existing port array data found, did you mean BpodLib.calibration.liquid.portarray.initialize(BpodSystem) ?")
+end
 BpodLib.calibration.liquid.LiquidCalibratorUI(BpodSystem.CalibrationTables.PortArrays, 'BpodSystem', BpodSystem, 'savepath', savepath)
