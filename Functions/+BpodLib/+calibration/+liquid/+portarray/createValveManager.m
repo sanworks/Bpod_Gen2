13ed9ec6d02d12aa62e4379d72a24f968815ee1f
George Stuyt <stugeor@hotmail.com>
2025-05-16 17:17:26 +1000

    Retrieve nModules dynamically

--- Functions/+BpodLib/+calibration/+liquid/+portarray/createValveManager.m
+++ Functions/+BpodLib/+calibration/+liquid/+portarray/createValveManager.m
@@ -2,13 +2,7 @@ function ValveManager = createValveManager(BpodSystem)
 % Create an empty ValveDataManager for PortArrays
 
 ValveManager = BpodLib.calibration.liquid.ValveDataManagerClass();
-switch BpodSystem.MachineType
-    % todo: add values for other machine types
-    case 2; nModuleChannels = 4;
-    case 3; nModuleChannels = 4;
-    case 4; nModuleChannels = 3;
-    otherwise; error('BpodLib:portarray.createValveManager:MachineTypeUnrecognised', 'Machine type unrecognised.')
-end
+nModuleChannels = numel(BpodSystem.Modules.Connected);
 
 for portarraynumber = 1:nModuleChannels
     for valveIndex = 1:4
