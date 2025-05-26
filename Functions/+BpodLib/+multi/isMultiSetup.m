b9bab8ea4106545c102f2fc1d6436dc3de5f6d43
George Stuyt <stugeor@hotmail.com>
2025-05-26 11:04:40 +1000

    Add LocalDir argument

--- Functions/+BpodLib/+multi/isMultiSetup.m
+++ Functions/+BpodLib/+multi/isMultiSetup.m
@@ -1,8 +1,17 @@
-function result = isMultiSetup(BpodSystem)
+function result = isMultiSetup(BpodSystem, varargin)
 % Determine if the system is being run on a computer with multiple state machines attached.
 
+p = inputParser();
+p.addParameter('LocalDir', '')
+p.parse(varargin{:})
+if isempty(p.Results.LocalDir)
+    localDir = BpodSystem.Path.LocalDir;
+else
+    localDir = p.Results.LocalDir;
+end
+
 % Currently the only way to check is if multiple liquid calibration files exist.
-filelist = dir(fullfile(BpodSystem.Path.LocalDir, 'Calibration Files'));
+filelist = dir(fullfile(localDir));
 validLogical = false(numel(filelist), 1);
 for idx = 1:numel(filelist)
     if ~filelist(idx).isdir
