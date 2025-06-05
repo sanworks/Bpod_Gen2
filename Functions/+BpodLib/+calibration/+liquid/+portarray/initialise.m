ef24754f448dce558a32e77d4b4b23108a253424
George Stuyt <stugeor@hotmail.com>
2025-06-05 16:23:01 +1000

    Fix handling of existing data

--- Functions/+BpodLib/+calibration/+liquid/+portarray/initialise.m
+++ Functions/+BpodLib/+calibration/+liquid/+portarray/initialise.m
@@ -1,9 +1,9 @@
 function initialise(BpodSystem)
 
 if isempty(BpodSystem.CalibrationTables.PortArrays)
-    error('BpodLib:PortArrayInitialise:ExistingData', 'PortArray data is already loaded in.')
-else
     BpodSystem.CalibrationTables.PortArrays = BpodLib.calibration.liquid.portarray.createValveManager(BpodSystem);
+else
+    error('BpodLib:PortArrayInitialise:ExistingData', 'PortArray data is already loaded in, maybe you want BpodLib.calibration.liquid.portarray.launchCalibrator(BpodSystem)')
 end
 
 BpodLib.calibration.liquid.portarray.launchCalibrator(BpodSystem)
