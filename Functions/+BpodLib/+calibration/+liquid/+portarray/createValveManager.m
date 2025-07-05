142c7842d7997b5b60df0612f1cb755d21c62770
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-07-05 14:40:52 +1000

    Batch format

--- Functions/+BpodLib/+calibration/+liquid/+portarray/createValveManager.m
+++ Functions/+BpodLib/+calibration/+liquid/+portarray/createValveManager.m
@@ -1,12 +1,42 @@
-function ValveManager = createValveManager(BpodSystem)
-% Create an empty ValveDataManager for PortArrays
+function PortArrayCal = createValveManager(BpodSystem)
+% Create an empty Port Array Module liquid calibration manager
+% PortArrayCal = createValveManager(BpodSystem)
+%
+% Arguments
+% ---------
+% BpodSystem : BpodObject
+%
+% Returns
+% -------
+% PortArrayCal : BpodLib.calibration.liquid.ValveDataManagerClass
+%     Manager pre-filled with empty eligible port array valves.
 
-ValveManager = BpodLib.calibration.liquid.ValveDataManagerClass();
+%{
+----------------------------------------------------------------------------
+
+This file is part of the Sanworks Bpod repository
+Copyright (C) Sanworks LLC, Rochester, New York, USA
+
+----------------------------------------------------------------------------
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed  WITHOUT ANY WARRANTY and without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
+PortArrayCal = BpodLib.calibration.liquid.ValveDataManagerClass();
 nModuleChannels = numel(BpodSystem.Modules.Connected);
 
-for portarraynumber = 1:nModuleChannels
+for moduleNumber = 1:nModuleChannels
     for valveIndex = 1:4
-        ValveManager.createValve(sprintf('PA%i_%i', portarraynumber, valveIndex));
+        PortArrayCal.createValve(sprintf('PA%i_%i', moduleNumber, valveIndex));
     end
 end
 
--- Functions/+BpodLib/+calibration/+liquid/+portarray/initialise.m
+++ Functions/+BpodLib/+calibration/+liquid/+portarray/initialise.m
@@ -1,8 +1,51 @@
 function initialise(BpodSystem)
+% Initialise a new port array calibration configuration
+% initialise(BpodSystem)
+%
+% Arguments
+% ---------
+% BpodSystem : BpodObject
+
+% docstring
+% signature
+%
+% Arguments
+% ---------
+% myarg : int
+%     Descriptor
+%
+% Keyword Arguments
+% -----------------
+% verbose : logical (default=true)
+%     Whether to display messages in Command Window.
+%
+% Returns
+% -------
+% completed : int
+%     Returns 1 if successful, 0 otherwise
+
+%{
+----------------------------------------------------------------------------
+
+This file is part of the Sanworks Bpod repository
+Copyright (C) Sanworks LLC, Rochester, New York, USA
+
+----------------------------------------------------------------------------
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed  WITHOUT ANY WARRANTY and without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%}
 
 if isempty(BpodSystem.CalibrationTables.PortArrays)
     BpodSystem.CalibrationTables.PortArrays = BpodLib.calibration.liquid.portarray.createValveManager(BpodSystem);
 else
     error('BpodLib:PortArrayInitialise:ExistingData', 'PortArray data is already loaded in, maybe you want BpodLib.calibration.liquid.portarray.launchCalibrator(BpodSystem)')
 end
-
