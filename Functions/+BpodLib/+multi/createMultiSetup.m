861da3c7de77b6ed3cafdae754addcd9cd6e2916
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-07-05 15:40:13 +1000

    Batch format

--- Functions/+BpodLib/+multi/createMultiSetup.m
+++ Functions/+BpodLib/+multi/createMultiSetup.m
@@ -1,9 +1,31 @@
 function createMultiSetup(BpodSystem)
-%createMultiSetup(BpodSystem)
-%   This function creates a new multi setup for the Bpod system.
-%   It either converts an existing single-setup folder structure to a multi-setup compatible one
-%   by moving the existing files, or creates a "fresh" setup by copying in example files to where
-%   the BpodSystem COM's configuration files are expected.
+% Create a new multi setup inside Bpod Local/
+% createMultiSetup(BpodSystem)
+%
+% This function creates a new multi setup for the Bpod system.
+% It either converts an existing single-setup folder structure to a multi-setup compatible one
+% by moving the existing files, or creates a "fresh" setup by copying in example files to where
+% the BpodSystem COM's configuration files are expected.
+
+%{
+----------------------------------------------------------------------------
+
+This file is part of the Sanworks Bpod repository
+Copyright (C) Sanworks LLC, Rochester, New York, USA
+
+----------------------------------------------------------------------------
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed  WITHOUT ANY WARRANTY and without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%}
 
 if BpodLib.path.compatibility.isLegacySettings(BpodSystem.Path.LocalDir)
     error('BpodLib:MultiSetup:LegacyIncompatible', 'Legacy setups must first be converted to modern setup with Config/ folder.')
--- Functions/+BpodLib/+path/verifyPathing.m
+++ Functions/+BpodLib/+path/verifyPathing.m
@@ -1,6 +1,45 @@
 function verified = verifyPathing(BpodSystem, varargin)
-% verified = verifyPathing(BpodSystem)
 % Verify that the expected pathing exists for the BpodSystem
+% verified = verifyPathing(BpodSystem)
+
+% docstring
+% signature
+%
+% Arguments
+% ---------
+% BpodSystem : BpodObject
+%
+% Keyword Arguments
+% -----------------
+% ThrowErrors : logical (default=true)
+%     Whether to throw errors if verification fails.
+% verbose : logical (default=true)
+%     Whether to display messages in Command Window.
+%
+% Returns
+% -------
+% verified : logical
+%     Returns true if successful, 0 otherwise
+
+%{
+----------------------------------------------------------------------------
+
+This file is part of the Sanworks Bpod repository
+Copyright (C) Sanworks LLC, Rochester, New York, USA
+
+----------------------------------------------------------------------------
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed  WITHOUT ANY WARRANTY and without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%}
 
 p = inputParser();
 p.addParameter('ThrowErrors', true)
