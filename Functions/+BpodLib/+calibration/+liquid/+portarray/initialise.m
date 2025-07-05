2eab122bceac24dd5c0bb7ebb0deb9ae155c81a1
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-07-05 14:39:14 +1000

    Remove UI launch

--- Functions/+BpodLib/+calibration/+liquid/+portarray/initialise.m
+++ Functions/+BpodLib/+calibration/+liquid/+portarray/initialise.m
@@ -6,4 +6,3 @@ else
     error('BpodLib:PortArrayInitialise:ExistingData', 'PortArray data is already loaded in, maybe you want BpodLib.calibration.liquid.portarray.launchCalibrator(BpodSystem)')
 end
 
-BpodLib.calibration.liquid.portarray.launchCalibrator(BpodSystem)
