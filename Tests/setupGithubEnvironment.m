4ffb9c92d72624c562b6eb1c400534da22f3b017
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-08-02 16:31:04 +1000

    Refactor environment setup into func

--- Tests/setupGithubEnvironment.m
+++ Tests/setupGithubEnvironment.m
@@ -1,11 +1,7 @@
 %% Prepare GitHub environment
 % Create Bpod_Local folder
 parentDir = fileparts(pwd);
-localDir = fullfile(parentDir, 'Bpod Local');
-calFolder = fullfile(localDir, 'Calibration Files');
-mkdir(localDir)
-mkdir(calFolder)
-copyfile(fullfile(parentDir, 'Bpod_Gen2/Examples/Example Calibration Files'), calFolder)
+addpath('Tests')
 
 % Bpod EMU
 % EndBpod
