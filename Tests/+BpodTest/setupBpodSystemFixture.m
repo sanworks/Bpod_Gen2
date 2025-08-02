2ef0b7754d672bc391994f48ce733a299b8f934e
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-08-02 16:25:10 +1000

    Create BpodSystem setup and teardown

---
+++ Tests/+BpodTest/setupBpodSystemFixture.m
@@ -0,0 +1,40 @@
+function setupBpodSystemFixture(testCase, varargin)
+% Prepare a BpodSystem for testing, using an existing BpodSystem or creating a new one.
+% setupBpodSystemFixture(testCase)
+%
+% Expected to be called in setupOnce()
+% Works in tandem with teardownBpodSystemFixture.m
+%
+% Keyword Arguments
+% -----------------
+% gui : bool (default = false)
+%     Startup a GUI component
+
+p = inputParser();
+p.addParameter('gui', false)
+p.parse(varargin{:})
+
+global BpodSystem
+wasRunning = ~isempty(BpodSystem);
+testCase.TestData.Original = struct();
+testCase.TestData.Original.wasRunning = wasRunning;
+if wasRunning
+    assert(isa(BpodSystem, 'BpodObject'), 'BpodSystem should be a BpodObject')
+    testCase.TestData.Original.BpodSystem = BpodSystem;
+    testCase.TestData.Original.LocalDir = BpodSystem.Path.LocalDir; % store for restoration later
+else
+    % Create a new BpodSystem for testing
+    % A fresh startup requires a local dir (to not contaminate existing local dir)
+    localDir = fullfile(tempname, 'Bpod Local');
+    mkdir(localDir)
+    testCase.TestData.Original.rootDir = fileparts(localDir);
+    BpodSystem = BpodTest.setupEmulator(localDir, 'gui', p.Results.gui);
+end
+
+if p.Results.gui
+    assert(~isempty(BpodSystem.GUIHandles))
+end
+
+testCase.TestData.BpodSystem = BpodSystem;
+
+end
---
+++ Tests/+BpodTest/teardownBpodSystemFixture.m
@@ -0,0 +1,21 @@
+function teardownBpodSystemFixture(testCase)
+% Reset the BpodSystem fixture to its original state
+% teardownBpodSystemFixture(testCase)
+%
+% Expected to be called in teardownOnce()
+% Works in tandem with setupBpodSystemFixture.m
+
+% Shut down the emulated Bpod setup if it was running
+if ~testCase.TestData.Original.wasRunning
+    EndBpod % clears global BpodSystem
+    rmdir(testCase.TestData.Original.rootDir, 's')
+end
+
+% Restore the original BpodSystem to its original state
+if testCase.TestData.Original.wasRunning
+    global BpodSystem
+    BpodSystem = testCase.TestData.Original.BpodSystem;
+    BpodLib.BpodObject.setup.updatePathAndSettings(BpodSystem, 'LocalDir', testCase.TestData.Original.LocalDir)
+end
+
+end
