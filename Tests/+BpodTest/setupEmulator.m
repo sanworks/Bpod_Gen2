9ba43da99e6d85682fd0ee7930ef15fff3dbc9c0
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-08-02 17:02:59 +1000

    Improve ordering

--- Tests/+BpodTest/setupEmulator.m
+++ Tests/+BpodTest/setupEmulator.m
@@ -2,10 +2,13 @@ function BpodSystem = setupEmulator(varargin)
 % Create a no-GUI Bpod Emulator
 % BpodSystem = setupEmulator(_)
 %
-% Keyword Arguments
-% -----------------
+% Arguments
+% ---------
 % LocalDir : char
 %     Location of the Bpod Local/, should be a tempname
+%
+% Keyword Arguments
+% -----------------
 % gui : bool (default = false)
 %     Use .InitializeGUI() method
 %
@@ -20,12 +23,12 @@ p.addParameter('gui', false)
 p.parse(varargin{:})
 
 BpodSystem = BpodObject('verbose', false);
-BpodSystem.EmulatorMode = 1;
+BpodSystem.EmulatorMode = true;
 BpodLib.BpodObject.setup.updatePathAndSettings(BpodSystem, 'verbose', false, 'LocalDir', p.Results.LocalDir)
+BpodSystem.SetupHardware();
 if p.Results.gui
     BpodSystem.InitializeGUI();
 end
-BpodSystem.SetupHardware();
 BpodSystem.Status.Initialized = true;
 
 end
