c7481e6d017687dfa5faffa7cfb9511abc2dbf35
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-11-24 23:15:20 +1100

    Improve messaging

--- Functions/+BpodLib/+calibration/+liquid/+ui/VerifyCOMGUI.m
+++ Functions/+BpodLib/+calibration/+liquid/+ui/VerifyCOMGUI.m
@@ -34,13 +34,23 @@ if BpodLib.multi.isMultiSetup(BpodSystem)
 
     % Multi setup - ask user which machine to import from.
     prompt = {
-        sprintf('The Bpod COM port (%s) does not match the', BpodLib.utils.getCurrentCOM(BpodSystem));
+        sprintf('The Bpod COM port (%s) does not match', BpodLib.utils.getCurrentCOM(BpodSystem));
         sprintf('liquid calibration file''s COM port (%s).', LiquidCal.metadata.COM);
         'Please select which COM to copy in the liquid';
         'calibration file from:'
     };
     ignoreOption = 'I will remember to recalibrate. (ignore)';
-    options = [{ignoreOption}, availableMachines'];
+    machineNames = [{ignoreOption}, availableMachines'];
+    options = machineNames;
+
+    % Replace thisMachineName with '(current machine)' in the list
+    [~, thisMachineName] = fileparts(BpodLib.path.getPath('config', BpodSystem))
+    for i = 1:length(options)
+        if strcmp(options{i}, thisMachineName)
+            options{i} = sprintf('%s (use existing file)', thisMachineName);
+            break
+        end
+    end
 
     [selectionIdx, ok] = listdlg('PromptString', prompt, ...
         'SelectionMode', 'single', ...
@@ -51,7 +61,7 @@ if BpodLib.multi.isMultiSetup(BpodSystem)
         return
     end
 
-    selectedMachine = options{selectionIdx};
+    selectedMachine = machineNames{selectionIdx};
     if strcmp(selectedMachine, ignoreOption)
         return
     end
